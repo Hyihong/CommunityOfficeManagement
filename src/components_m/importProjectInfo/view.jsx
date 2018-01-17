6e5b6919343410ebe8ce48fbb100e3d3dd580d33
chenyihong <496695119@qq.com>
2018-01-17 15:26:57 +0800

    【移动端】新增业主信息导入模块的框架

---
+++ src/components_m/importProjectInfo/view.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { actions as projectListAction } from '../projectList'
+import { Return } from '../shared';
+import { getQueryString } from '../../tools/baseTools'
+//import $ from 'jquery'
+//import 'jquery-form'
+import './style.less'
+
+
+class ImportProjectInfo extends React.Component {
+   constructor(){
+     super()
+     this.state = {}
+   }
+   componentDidMount(){
+      const projectID = getQueryString( this.props.location.search,'ID' );   
+   }
+      
+
+    render(){ 
+        return (
+            <div>
+                文件上传页面
+            </div>
+        );
+      }
+    };
+    
+
+const mapStateToProps = (state) => {
+    const projects = state.projectList;
+    return{
+      
+    }
+}
+
+
+//或者
+const mapDispatchToProps = (dispatch) => ({
+  
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ImportProjectInfo));
